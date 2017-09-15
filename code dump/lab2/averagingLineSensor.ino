66e0cdc640846a5ab79571558449acf59cce1c08
katherinejlu <kl645@cornell.edu>
2017-09-15 12:00:34 -0400

    maybe it works i dunno

---
+++ code dump/lab2/averagingLineSensor.ino
@@ -0,0 +1,86 @@
+//Code for the QRE1113 Analog board
+//Outputs via the serial terminal – Lower numbers mean more reflected
+#include <Servo.h>
+int leftPin = A0; //connected to analog 0
+int midPin = A1; //connected to analog 0
+int rightPin = A2; //connected to analog 0
+Servo servoL;
+Servo servoR;
+int line[3]; //array to maintain line location betwee n3 sensors
+/*
+ * line[0] = left sensro
+ * line[1] = middle sensor
+ * line[2] = right sensor
+ */
+int leftSpeed=180;
+int rightSpeed=0; //assume right servo is in backwards orientation 
+
+/*for sensor ranges:
+ * on black: >800
+ * on white: <300
+ */
+static int black = line[1];
+static int white = line[1] - threshold; //high end white lineue
+static int threshold = (2*line[1] - (line[0] + line[2]))/2; //average difference between white and black lineues
+
+void setup() {
+  Serial.begin(9600);
+  servoL.attach(9);
+  servoR.attach(11);
+}
+
+void loop() {
+  servoL.write(leftSpeed);
+  servoR.write(rightSpeed);
+  line[0] = analogRead(leftPin);
+  line[1] = analogRead(midPin);
+  line[2] = analogRead(rightPin);
+//  Serial.print(line[0]);
+//  Serial.print("\t");
+//  Serial.print(line[1]);
+//  Serial.print("\t"); 
+//  Serial.println(line[2]);
+
+  goStraight(line);
+  if(isJunction(line)) {  //for now, robot just stops when it reaches a juncition.
+    Serial.println("at Junction");
+    leftSpeed =90;
+    rightSpeed =90;
+  }
+//  delay(1000);
+}
+
+
+boolean goStraight(int line[3]){
+  if(isLeft(line)){
+    Serial.println("is Left of line");
+    rightSpeed -=5;
+  }
+  else if(isRight(line)){
+    leftSpeed -=5;
+    Serial.println("is Right of line");
+  }
+  else if(line[0]<=white && line[2]<=white){
+    //line[1] - line[0] >= threshold && line[1]-line[2] >= threshold){
+    Serial.println("is straight");
+    leftSpeed = 180;
+    rightSpeed = 0;
+  }
+}
+
+boolean isRight(int line[3]){
+  return(line[0]>white && line[2]<=white);
+//  return(line[0]- line[2] >= threshold);
+}
+
+boolean isLeft(int line[3]){
+  return(line[2]>white && line[2]<=white);
+//return(line[2] - line[0] >= threshold);
+}
+
+boolean isJunction(int line[3]){
+  return (line[0]>white && line[1] >white && line[2]>white);
+  //return (abs(line[0] - line[1]) <= threshold  && abs(line[1] - line[2]) <= threshold );
+}
+
+//void turnLeft(
