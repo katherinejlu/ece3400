6ec775410d85702d8234d812e150658673541afb
katherinejlu <kl645@cornell.edu>
2017-09-15 15:13:54 -0400

    static variable white still changes for some reason

--- code dump/lab2/averagingLineSensor.ino
+++ code dump/lab2/averagingLineSensor.ino
@@ -6,43 +6,59 @@ int midPin = A1; //connected to analog 0
 int rightPin = A2; //connected to analog 0
 Servo servoL;
 Servo servoR;
-int line[3]; //array to maintain line location betwee n3 sensors
+int line[2]; //array to maintain line location betwee n3 sensors
 /*
  * line[0] = left sensro
  * line[1] = middle sensor
  * line[2] = right sensor
  */
-int leftSpeed=180;
-int rightSpeed=0; //assume right servo is in backwards orientation 
+int leftSpeed=135;
+int rightSpeed=45; //assume right servo is in backwards orientation 
 
 /*for sensor ranges:
  * on black: >800
  * on white: <300
  */
-static int black = line[1];
-static int white = line[1] - threshold; //high end white lineue
-static int threshold = (2*line[1] - (line[0] + line[2]))/2; //average difference between white and black lineues
+
+static int black; static int white;
 
 void setup() {
   Serial.begin(9600);
   servoL.attach(9);
   servoR.attach(11);
+  line[0] = analogRead(leftPin);
+  line[1] = analogRead(midPin);
+  line[2] = analogRead(rightPin);
+  black = line[1];
+//static int threshold = (2*line[1] - (line[0] + line[2]))/2; //average difference between white and black lineues
+  white = (line[0]+line[2])/2 +100; //high end white lineue
+
 }
 
 void loop() {
+  Serial.print("\t");
+  Serial.print("this is black:");
+  Serial.print("\t");
+  Serial.print(black);
+  Serial.print("\t");
+  Serial.print("this is white:");
+  Serial.print("\t");
+  Serial.print(white);
+  Serial.print("\t");
+
   servoL.write(leftSpeed);
   servoR.write(rightSpeed);
   line[0] = analogRead(leftPin);
   line[1] = analogRead(midPin);
   line[2] = analogRead(rightPin);
-//  Serial.print(line[0]);
-//  Serial.print("\t");
-//  Serial.print(line[1]);
-//  Serial.print("\t"); 
-//  Serial.println(line[2]);
+  Serial.print(line[0]);
+  Serial.print("\t");
+  Serial.print(line[1]);
+  Serial.print("\t"); 
+  Serial.println(line[2]);
+  goStraight();
 
-  goStraight(line);
-  if(isJunction(line)) {  //for now, robot just stops when it reaches a juncition.
+  if(isJunction()) {  //for now, robot just stops when it reaches a juncition.
     Serial.println("at Junction");
     leftSpeed =90;
     rightSpeed =90;
@@ -51,35 +67,38 @@ void loop() {
 }
 
 
-boolean goStraight(int line[3]){
-  if(isLeft(line)){
+boolean goStraight(){
+  if(isLeft()){
     Serial.println("is Left of line");
-    rightSpeed -=5;
+    leftSpeed =140;
   }
-  else if(isRight(line)){
-    leftSpeed -=5;
+  else if(isRight()){
+    rightSpeed =50;
     Serial.println("is Right of line");
   }
-  else if(line[0]<=white && line[2]<=white){
+  else if((abs(line[1] - line[2])>=200) && (abs(line[1] - line[0])>=200)){
     //line[1] - line[0] >= threshold && line[1]-line[2] >= threshold){
     Serial.println("is straight");
-    leftSpeed = 180;
-    rightSpeed = 0;
+    leftSpeed = 135;
+    rightSpeed = 45;
   }
 }
 
-boolean isRight(int line[3]){
-  return(line[0]>white && line[2]<=white);
+boolean isRight(){
+    return(line[0] - line[2]>=200);
+ // return(line[0]>white && line[2]<=white);
 //  return(line[0]- line[2] >= threshold);
 }
 
-boolean isLeft(int line[3]){
-  return(line[2]>white && line[2]<=white);
+boolean isLeft(){
+  return(line[2] - line[0]>=200);
+ // return(line[2]>white && line[2]<=white);
 //return(line[2] - line[0] >= threshold);
 }
 
-boolean isJunction(int line[3]){
-  return (line[0]>white && line[1] >white && line[2]>white);
+boolean isJunction(){
+  return ((abs(line[1] - line[2])<=200) && (abs(line[1] - line[0])<=200)); 
+  //return (line[0]>white && line[1] >white && line[2]>white);
   //return (abs(line[0] - line[1]) <= threshold  && abs(line[1] - line[2]) <= threshold );
 }
 
